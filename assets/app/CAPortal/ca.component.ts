d7445d9dc114e24289987d482307da8cc64bd155
Shiv Tavker <tavkershiv@gmail.com>
2017-10-08 15:41:55 +0530

    Update ca.component.ts

--- assets/app/CAPortal/ca.component.ts
+++ assets/app/CAPortal/ca.component.ts
@@ -3,10 +3,16 @@ import {Component} from '@angular/core';
 @Component({
     selector:'page-ca',
     templateUrl: './ca.component.html',
-    styleUrls: ['./ca.component.css']
+    styleUrls: ['../../../public/stylesheets/ca.component.css']
 })
 
 export class CAComponent{
+isLoaded = false;
+
+    ngOnInit(){
+        this.isLoaded = true;
+    }
+
     enter(){
         console.log('entered');
     }
